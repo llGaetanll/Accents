8f8785a39bc1fc63d55f4f3586f46eaedc251dbe
llGaetanll <gaetan.almela@gmail.com>
2022-06-10 19:12:42 +0100

    removed redundant code
    since moved to different file

--- src/components/popup.js
+++ src/components/popup.js
@@ -4,29 +4,6 @@
 
 import Accents from "./accents";
 
-const AccentButton = ({ accent, shortcut }) => {
-  return (
-    <button
-      css={{
-        display: "flex",
-        flexDirection: "column",
-
-        width: 50,
-        height: 70,
-
-        cursor: "pointer",
-        border: 0,
-        padding: 0,
-        borderRadius: 10,
-        background: "white",
-      }}
-    >
-      <h1 css={{ flex: 1, textAlign: "center", margin: 0 }}>{accent}</h1>
-      <h2 css={{ flex: 1, textAlign: "center", margin: 0 }}>{shortcut}</h2>
-    </button>
-  );
-};
-
 const Popup = () => {
   return (
     <div
