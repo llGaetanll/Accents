f9998b8688876d15dde25b13f009cc68157cb2c2
llGaetanll <gaetan.almela@gmail.com>
2022-06-14 13:03:58 +0200

    all accent keybinds now mostly work

--- src/components/accents/KeyListener.js
+++ src/components/accents/KeyListener.js
@@ -4,6 +4,7 @@ import { ModalContext } from "../../util/context";
 import { useAsyncReducer } from "../../util/index";
 import { getTextAreaCaret } from "../../util/caret";
 import { ACCENTS } from "../../util/data";
+import { setChar } from "../../util/text";
 
 // the default state of our async reducer
 const defState = {
@@ -29,6 +30,12 @@ const keybindReducer = (state, { type, event }) => {
         keyPresses: state.keyPresses.filter((keyPress) => keyPress.key !== key),
       };
     }
+    case "CLEAR_KEYPRESSES": {
+      return {
+        ...state,
+        keyPresses: [],
+      };
+    }
 
     case "START_TIMEOUT": {
       const { time, timeoutCallback } = event;
@@ -52,8 +59,20 @@ const keybindReducer = (state, { type, event }) => {
   }
 };
 
+const keybindList = [
+  ...Object.keys(ACCENTS),
+  ...[].concat.apply(
+    [],
+    Object.keys(ACCENTS).map((accent) =>
+      ACCENTS[accent].map((_, i) => [accent, i + 1].join(""))
+    )
+  ),
+];
+
+// console.log(keybindList);
+
 const KeyListener = ({ children, setShow }) => {
-  const { display, hide, show } = useContext(ModalContext);
+  const { display, key: modalKey, hide, show } = useContext(ModalContext);
 
   const [keybindState, dispatchKeybind] = useAsyncReducer(
     keybindReducer,
@@ -81,30 +100,63 @@ const KeyListener = ({ children, setShow }) => {
 
   // called whenever presses a key
   const keyDownHandler = useCallback(
-    ({ key, targetEl }) =>
-      (dispatchKeybind, getState) => {
-        if (display) {
-          hide();
-        } else {
-          // check if the key is "accentable"
-          // if so, start a timeout that resolves to `displayModal`
-          if (key in ACCENTS)
-            return dispatchKeybind([
-              {
-                type: "START_TIMEOUT",
-                event: {
-                  time: MIN_PRESS_MS,
-                  timeoutCallback: () => {
-                    clearTimeout(getState().timeout);
+    (event) => (dispatchKeybind, getState) => {
+      const { key, target: targetEl } = event;
 
-                    displayModal(key, targetEl);
-                  },
+      const keybind = getState().keyPresses.map((kp) => kp.key);
+      const keybindStr = keybind.join("");
+
+      // console.log(keybind, keybindStr, keybindList.indexOf(keybindStr));
+
+      // pressing any key hides the modal
+      if (display) hide();
+
+      // if the user is pressing a key combo that we care about
+      // TODO: detect if only last or last two keys are good instead
+      if (keybindList.indexOf(keybindStr) > -1) {
+        // if we use the keyboard shortcut for typing in the accent,
+        // type it in directly
+        if (keybind.length > 1) {
+          const [key, idx] = keybind;
+          const char = ACCENTS[key][idx - 1];
+
+          // type the key
+          setChar(targetEl, char);
+        } else {
+          dispatchKeybind([
+            {
+              type: "START_TIMEOUT",
+              event: {
+                time: MIN_PRESS_MS,
+                timeoutCallback: () => {
+                  clearTimeout(getState().timeout);
+
+                  displayModal(key, targetEl);
                 },
               },
-            ]);
+            },
+          ]);
         }
-      },
-    [display, hide, displayModal]
+      }
+
+      const numbers = ACCENTS[modalKey]?.map((_, i) => i);
+
+      // check for when the modal is visible and user presses only a number
+      if (
+        display &&
+        !isNaN(keybindStr) &&
+        numbers.indexOf(Number(keybindStr) - 1) > -1
+      ) {
+        // prevent typing in the number
+        event.preventDefault();
+
+        const char = ACCENTS[modalKey][Number(keybindStr) - 1];
+
+        // type the key
+        setChar(targetEl, char);
+      }
+    },
+    [display, modalKey, hide, displayModal]
   );
 
   // removes any timeouts
@@ -118,10 +170,16 @@ const KeyListener = ({ children, setShow }) => {
   // it calls keyUphandler and removes the given keypress
   const handleAddKeyUp = useCallback(
     ({ key, timeStamp }) => {
-      dispatchKeybind([
-        keyUpHandler({ key, timeStamp }),
-        { type: "REM_KEYPRESS", event: key },
-      ]); // handle keyPress and remove key
+      dispatchKeybind(keyUpHandler({ key, timeStamp }));
+
+      // Note that this will NOT detect the keyup of <key>
+      // when `meta + <key>` is pressed. This is known behavior, see:
+      //
+      // https://stackoverflow.com/questions/27380018
+      //
+      // Thus we clear the stack on a Meta keyup event
+      if (key === "Meta") dispatchKeybind({ type: "CLEAR_KEYPRESSES" });
+      else dispatchKeybind({ type: "REM_KEYPRESS", event: key });
     },
     [dispatchKeybind]
   );
@@ -130,21 +188,31 @@ const KeyListener = ({ children, setShow }) => {
   // it adds the given keypress and calls keyDownHandler which handles any further logic
   const handleAddKeyDown = useCallback(
     (event) => {
-      const { key, timeStamp, target } = event;
+      const { key, timeStamp } = event;
 
-      // if we were already pressing the current key, don't record it
-      if (keyPresses.findIndex((kp) => kp.key === key) >= 0) {
-        // if the key is an accents key, we don't wanna double type it
-        if (key in ACCENTS) event.preventDefault();
+      const prevKeybinds = keyPresses.map((kp) => kp.key);
 
-        return;
-      }
+      // whether we've seen that key pressed yet
+      const seen = prevKeybinds.indexOf(key) > -1;
 
-      // add key to keyPresses and call handler with newest state
-      dispatchKeybind([
-        { type: "ADD_KEYPRESS", event: { key, timeStamp } },
-        keyDownHandler({ key, targetEl: target }),
-      ]);
+      // include the current key
+      const allKeybinds = seen ? prevKeybinds : [...prevKeybinds, key];
+      const keybindStr = allKeybinds.join("");
+
+      // only record keys we've not seen before
+      if (!seen)
+        dispatchKeybind({ type: "ADD_KEYPRESS", event: { key, timeStamp } });
+
+      // if the user is pressing a key combo that we care about
+      // TODO: detect if only last or last two keys are good instead
+      const accentKeybind = keybindList.indexOf(keybindStr) > -1;
+
+      // key combo must be valid, we prevent any numbers from being typed, or type only one letter character
+      if (accentKeybind && (keybindStr.length > 1 || seen))
+        event.preventDefault();
+
+      // dont dispatch a new event if we've seen the key combo beforehand
+      if (!seen) dispatchKeybind(keyDownHandler(event));
     },
     [keyPresses, dispatchKeybind, keyDownHandler]
   );
